b476fadc2ddb9a546f28b9adf0b588bfbefb944a
macke945 <55479431+macke945@users.noreply.github.com>
2020-10-16 09:19:27 +0200

    added global styles and themes

--- portfolio-frontend/gatsby-browser.js
+++ portfolio-frontend/gatsby-browser.js
@@ -1,1 +1,19 @@
 import "./src/css/main.css"
+import Theme from "./src/themes/theme"
+import React from "react"
+import {createGlobalStyle, ThemeProvider} from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+font-family: ${props => props.theme.fonts.ffsecondary};
+background: ${props => props.theme.colors.clrwhite};
+color: var${props => props.theme.colors.clrgrey1};
+line-height: 1.5;
+font-size: 0.875rem;
+margin-top: 5rem;
+`
+export const wrapRootElement = ({ element }) => (
+    <ThemeProvider theme={Theme}>
+        <GlobalStyles />
+        {element}
+    </ThemeProvider>
+)
